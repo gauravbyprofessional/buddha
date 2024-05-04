d5a1f87b85a56848a2ceaf2e9fef51e3602e1d6b
Gaurav Kumar <gaurav7professional@gmail.com>
2024-05-04 09:16:34 +0530

    This is my first commit

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 <h1>Hello world!</h1>
 <p>This is the first file in my new Git Repo.</p>
-
+<p>I have add new line here.</p>
+<img src="image.jpg" alt="Girl in a jacket" width="500" height="600">
 </body>
 </html>
